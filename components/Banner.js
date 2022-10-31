41e56a56cd3333fe56bc460532b205ea27041692
Isaiah Desta <isaiahdesta@Isaiahs-MBP.attlocal.net>
2022-10-31 14:22:02 -0400

    padding issue on banner.

--- components/Banner.js
+++ components/Banner.js
@@ -7,8 +7,9 @@ const Banner = () => {
       <Image
         src="https://links.papareact.com/0fm"
         width={10000}
-        height={10000}
+        height={700}
         object-fit="cover"
+        layout="fill"
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
