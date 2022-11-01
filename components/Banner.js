22ff117eb30cc3f7f71e9ca3b90274cc4d11db8b
Isaiah Desta <isaiahdesta@Isaiahs-MacBook-Pro.local>
2022-11-01 09:18:57 -0400

    banner complete

--- components/Banner.js
+++ components/Banner.js
@@ -8,8 +8,7 @@ const Banner = () => {
         src="https://links.papareact.com/0fm"
         width={10000}
         height={700}
-        object-fit="cover"
-        layout="fill"
+        className="w-full h-full object-cover"
       />
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
